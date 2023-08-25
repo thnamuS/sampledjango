ed875b36a25bf1e8a90d15eb3808cc6eb8232e0c
thnamuS?git config --global user.name thnamuS? <sumanthmahantu@gmail.com>
2023-08-25 16:31:31 +0530

    hackathon website to deploy

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i%%es1rng^-fnt$1vq&e080*7fl4nlwavi)=8+sb3#wnx5c%ks
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.235.103.24','localhost','0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','0.0.0.0']
 
 
 # Application definition
