e5d042e61ea04754b20ee473ed4053ebeb48d8f3
Sumanth <118098664+thnamuS@users.noreply.github.com>
2023-08-25 23:36:18 +0530

    Update settings.py

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i%%es1rng^-fnt$1vq&e080*7fl4nlwavi)=8+sb3#wnx5c%ks
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','localhost','0.0.0.0']
+ALLOWED_HOSTS = ['13.127.47.120','localhost','0.0.0.0']
 
 
 # Application definition
